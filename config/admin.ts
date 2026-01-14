512fb46693cdc7ad8170cd2ac994754e240dcb8c
OGBAJI MICHAEL <ogbaji201@gmail.com>
2026-01-14 06:10:08 +0000

    Configure Postgres (Neon)for Strapi on Railway

--- config/admin.ts
+++ config/admin.ts
@@ -1,7 +1,31 @@
-export default ({ env }) => ({
+// export default ({ env }) => ({
+//   auth: {
+//     secret: env('ADMIN_JWT_SECRET'),
+//   },
+//   apiToken: {
+//     salt: env('API_TOKEN_SALT'),
+//   },
+//   transfer: {
+//     token: {
+//       salt: env('TRANSFER_TOKEN_SALT'),
+//     },
+//   },
+//   secrets: {
+//     encryptionKey: env('ENCRYPTION_KEY'),
+//   },
+//   flags: {
+//     nps: env.bool('FLAG_NPS', true),
+//     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+//   },
+// });
+
+// config/admin.js
+module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
+
+  // recommended for production stability:
   apiToken: {
     salt: env('API_TOKEN_SALT'),
   },
@@ -9,12 +33,5 @@ export default ({ env }) => ({
     token: {
       salt: env('TRANSFER_TOKEN_SALT'),
     },
-  },
-  secrets: {
-    encryptionKey: env('ENCRYPTION_KEY'),
-  },
-  flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
   },
 });
