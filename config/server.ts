fe12a18674120fc145eb15ed1d0c076bac0ae541
OGBAJI MICHAEL <ogbaji201@gmail.com>
2026-01-16 16:53:22 +0000

    Add Cloudinary upload support

--- config/server.ts
+++ config/server.ts
@@ -9,7 +9,11 @@
 
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 8080),
+  port: env.int('PORT', 1337), // keep your Railway port if you want 8080
   url: env('PUBLIC_URL'),
   proxy: true,
+
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
 });
