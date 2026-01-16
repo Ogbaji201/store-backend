c61af598808b4a8f6ad9a259c9044ffe2576e22d
OGBAJI MICHAEL <ogbaji201@gmail.com>
2026-01-16 16:30:30 +0000

    Add Cloudinary upload support

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,9 +1,23 @@
+
 // export default [
-//   'strapi::logger',
 //   'strapi::errors',
 //   'strapi::security',
-//   'strapi::cors',
+//   {
+//     name: 'strapi::cors',
+//     config: {
+//       origin: [
+//         'https://cheryblisshealth.com',
+//         'https://store-backend-production-0858.up.railway.app',
+//         // add your Vercel frontend too:
+//         // 'https://your-frontend.vercel.app',
+//       ],
+//       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+//       credentials: true,
+//       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+//     },
+//   },
 //   'strapi::poweredBy',
+//   'strapi::logger',
 //   'strapi::query',
 //   'strapi::body',
 //   'strapi::session',
@@ -14,19 +28,52 @@
 
 export default [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com'  // Add Cloudinary
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com'  // Add Cloudinary
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   {
     name: 'strapi::cors',
     config: {
       origin: [
+        'http://localhost:1338',
+        'http://localhost:3001',
         'https://cheryblisshealth.com',
         'https://store-backend-production-0858.up.railway.app',
-        // add your Vercel frontend too:
-        // 'https://your-frontend.vercel.app',
       ],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      headers: [
+        'Content-Type',
+        'Authorization',
+        'Origin',
+        'Accept',
+        'X-Requested-With',
+        'Access-Control-Allow-Headers'
+      ],
       credentials: true,
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+      keepHeaderOnError: true,
     },
   },
   'strapi::poweredBy',
--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,15 @@
+// export default ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+// });
+
+
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
+  port: env.int('PORT', 8080),
+  url: env('PUBLIC_URL'),
+  proxy: true,
 });
